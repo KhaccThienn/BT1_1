1eb401a67bd1315715bc0809c307c504fb98ba85
KhacThien <94694945+KhaccThienn@users.noreply.github.com>
2022-05-15 21:04:27 +0700

    Add files via upload

---
+++ B1 - 1/src/app.ts
@@ -0,0 +1,43 @@
+var person: { name: string, email: string, phone: string, gender: string, favourite: string }[] = [
+    {
+        name: 'Le Khac Thien',
+        email: 'muzankibut112@gmail.com',
+        phone: '0392689213' ,
+        gender: 'nam',
+        favourite:'gaming, mixing, watching'
+    },
+];
+var _html = '';
+for (let key in person) {
+    _html += `
+            <table border="1" cellspacing="0" cellpadding="5">
+                <thead>
+                    Thong Tin
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="bold">Ho va Ten</td>
+                        <td>${person[key].name}</td>
+                    </tr>
+                    <tr>
+                        <td class="bold">Email</td>
+                        <td>${person[key].email}</td>
+                    </tr>
+                    <tr>
+                        <td class="bold">SDT</td>
+                        <td>${person[key].phone}</td>
+                    </tr>
+                    <tr>
+                        <td class="bold">Gender</td>
+                        <td>${person[key].gender}</td>
+                    </tr>
+                    <tr>
+                        <td class="bold">Favourites</td>
+                        <td>${person[key].favourite}</td>
+                    </tr>
+                </tbody>
+            </table>
+        `;
+}
+var roots = document.getElementById('root');
+roots.innerHTML = _html;
